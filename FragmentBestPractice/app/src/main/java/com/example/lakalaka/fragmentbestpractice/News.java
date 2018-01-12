85a6d5e195746fcfe721a5c6d122ec5dc443f39c
lakalaka <718326384@qq.com>
2018-01-12 16:57:43 +0800

    FragmentBestPractice

---
+++ FragmentBestPractice/app/src/main/java/com/example/lakalaka/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.lakalaka.fragmentbestpractice;
+
+/**
+ * Created by lakalaka on 2018/1/12/0012.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    String getTitle() {
+        return title;
+    }
+
+    String getContent() {
+        return content;
+    }
+
+    void setTitle(String title) {
+        this.title = title;
+    }
+
+    void setContent(String content) {
+        this.content = content;
+    }
+}

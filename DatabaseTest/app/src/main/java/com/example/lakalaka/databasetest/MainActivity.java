a7e0dd95cec706ed52dedc9d83b11eb5935c21a9
lakalaka <718326384@qq.com>
2018-01-16 11:31:17 +0800

    add provider content

--- DatabaseTest/app/src/main/java/com/example/lakalaka/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/lakalaka/databasetest/MainActivity.java
@@ -3,83 +3,115 @@ package com.example.lakalaka.databasetest;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-        private MyDatabaseHelper dbHelper;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MyDatabaseHelper dbHelper;
+    private String newId;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper=new MyDatabaseHelper(this,"BookStore.db",null,2);
-        Button createDatabase= (Button) findViewById(R.id.create_database);
-        Button addData= (Button) findViewById(R.id.add_data);
-        Button updateData= (Button) findViewById(R.id.update_data);
-        Button deleteData= (Button) findViewById(R.id.delete_data);
-        Button queryData= (Button) findViewById(R.id.query_data);
-        queryData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SQLiteDatabase db=dbHelper.getWritableDatabase();
-                Cursor cursor=db.query("Book",null,null,null,null,null,null);
-                if(cursor.moveToFirst()){
-                    do{
-                        String name=cursor.getString(cursor.getColumnIndex("name"));
-                        String author=cursor.getString(cursor.getColumnIndex("author"));
-                        int pages=cursor.getInt(cursor.getColumnIndex("pages"));
-                        double price=cursor.getDouble(cursor.getColumnIndex("price"));
-                        Log.d("Tag", "book name is "+name);
-                        Log.d("Tag", "book author is "+author);
-                        Log.d("Tag", "book pages is "+pages);
-                        Log.d("Tag", "book price is "+price);
-                    }while (cursor.moveToNext());
+        findViewById(R.id.create_database).setOnClickListener(this);
+        findViewById(R.id.add_data).setOnClickListener(this);
+        findViewById(R.id.update_data).setOnClickListener(this);
+        findViewById(R.id.delete_data).setOnClickListener(this);
+        findViewById(R.id.query_data).setOnClickListener(this);
+        findViewById(R.id.add_data_provider).setOnClickListener(this);
+        findViewById(R.id.delete_data_provider).setOnClickListener(this);
+        findViewById(R.id.update_data_provider).setOnClickListener(this);
+        findViewById(R.id.query_data_provider).setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 2);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        Uri uri=Uri.parse("content://com.example.lakalaka.databasetest.provider/book");
+        Uri uri1=Uri.parse("content://com.example.lakalaka.databasetest.provider/book/"+newId);
+        switch (v.getId()) {
+            case R.id.add_data:
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);
+                values.clear();
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+                break;
+            case R.id.update_data:
+                values.put("price", 10.99);
+                db.update("Book", values, "name = ? ", new String[]{"The Da Vinci Code"});
+                break;
+            case R.id.delete_data:
+                db.delete("Book", "pages>?", new String[]{"500"});
+                break;
+            case R.id.query_data:
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("Tag", "book name is " + name);
+                        Log.d("Tag", "book author is " + author);
+                        Log.d("Tag", "book pages is " + pages);
+                        Log.d("Tag", "book price is " + price);
+                    } while (cursor.moveToNext());
                 }
                 cursor.close();
-            }
-        });
-        deleteData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SQLiteDatabase db=dbHelper.getWritableDatabase();
-                db.delete("Book","pages>?",new String[]{"500"});
-            }
-        });
-        updateData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SQLiteDatabase db=dbHelper.getWritableDatabase();
-                ContentValues values=new ContentValues();
-                values.put("price",10.99);
-                db.update("Book",values,"name = ? ",new String[]{"The Da Vinci Code"});
-            }
-        });
-        addData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SQLiteDatabase db=dbHelper.getWritableDatabase();
-                ContentValues values=new ContentValues();
-                values.put("name","The Da Vinci Code");
-                values.put("author","Dan Brown");
-                values.put("pages",454);
-                values.put("price",16.96);
-                db.insert("Book",null,values);
-                values.clear();
-                values.put("name","The Lost Symbol");
-                values.put("author","Dan Brown");
-                values.put("pages",510);
-                values.put("price",19.95);
-                db.insert("Book",null,values);
-            }
-        });
-        createDatabase.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                break;
+            case R.id.create_database:
                 dbHelper.getWritableDatabase();
-            }
-        });
+                break;
+            case R.id.add_data_provider:
+                ContentValues valuse=new ContentValues();
+                values.put("name","A Clash of Kings");
+                values.put("author","Georger Martin");
+                values.put("pages",1040);
+                values.put("price",22.85);
+                Uri newUri=getContentResolver().insert(uri,values);
+                newId=newUri.getPathSegments().get(1);
+                break;
+            case R.id.delete_data_provider:
+                getContentResolver().delete(uri1,null,null);
+                break;
+            case R.id.update_data_provider:
+                values.put("name","A Srorm of Swords");
+                values.put("pages",1216);
+                values.put("price",24.05);
+                getContentResolver().update(uri1,values,null,null);
+                break;
+            case R.id.query_data_provider:
+                Cursor cursor1=getContentResolver().query(uri,null,null,null,null);
+                if(cursor1!=null){
+                    while (cursor1.moveToNext()){
+                        String name=cursor1.getString(cursor1.getColumnIndex("name"));
+                        String author=cursor1.getString(cursor1.getColumnIndex("author"));
+                        int pages=cursor1.getInt(cursor1.getColumnIndex("pages"));
+                        double price=cursor1.getDouble(cursor1.getColumnIndex("price"));
+                        Log.d("Tag", "book name is " + name);
+                        Log.d("Tag", "book author is " + author);
+                        Log.d("Tag", "book pages is " + pages);
+                        Log.d("Tag", "book price is " + price);
+                    }
+                    cursor1.close();
+                }
+                break;
+
+
+        }
+
+
     }
 }
--- DatabaseTest/app/src/main/java/com/example/lakalaka/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/lakalaka/databasetest/MyDatabaseHelper.java
@@ -3,7 +3,6 @@ package com.example.lakalaka.databasetest;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /**
  * Created by lakalaka on 2018/1/14/0014.
@@ -21,7 +20,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table Book(id integer primary key autoincrement,author text,price real,pages integer,name text);");
         db.execSQL("create table Category(id integer primary key autoincrement,category _name text,category_code integer);");
-        Toast.makeText(mcontext, "Create succeeded", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
